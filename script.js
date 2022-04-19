85cf1490e5f4102b77abf687c1f74b9edc87e286
joab <pbnjoab@gmail.com>
2022-04-18 22:21:26 -0400

    Finish playRound func

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-/*This function is the CPU player you are playing against. It will pick-
-    a random choice from either rock, paper, or scissor. */
 
-function computerPlay(string){
-    const weapons = ['rock', 'paper', 'scissors'];
-    const choice = weapons[Math.floor(Math.random()*weapons.length)];
-    return choice;
-    
+
+/*This function is the CPU player you are playing against. It will pick-
+    a random choice from either rock, paper, or scissors. */
+
+function computerPlay(){
+    let weapons = ['Rock', 'Paper', 'Scissors'];
+    let choice = weapons[Math.floor(Math.random()*weapons.length)];
+    return choice;   
 }
-console.log(computerPlay());
 
 /*This function will initiate the round that compares the player's input-
     and the computer's input and declare the winner for the SINGLE ROUND */
 
 function playRound(playerSelection,computerSelection){
-
+    if ((playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock")) {
+        return ('You Lose! '+ computerSelection+ ' beats '+ (playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()))
+    }
+    else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return ('Tied!')
+    }
+    else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors")){
+        return ('You Win! '+ (playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()) + ' beats '+ computerSelection)
+    }
 }
+
+const playerSelection = "PAPER"
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
