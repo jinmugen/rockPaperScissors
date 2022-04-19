b2f2687c1c53b1443bec56a3b307e0d717eafd1b
joab <pbnjoab@gmail.com>
2022-04-18 22:32:17 -0400

    Fix win logic

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function playRound(playerSelection,computerSelection){
     else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         return ('Tied!')
     }
-    else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") ||
-    (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
-    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors")){
+    else if ((playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") ||
+    (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors")){
         return ('You Win! '+ (playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()) + ' beats '+ computerSelection)
     }
 }
 
-const playerSelection = "PAPER"
+const playerSelection = "sCISSors"
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
